0d49822861c1c3621a4fe67222e5107b128352ae
SEOKMINJUN <smj00316@hanyang.ac.kr>
2025-05-30 21:34:56 +0900

    Rename project name to navi

--- navi/urls.py
+++ navi/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for hy3d_project project.
+URL configuration for navi project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
