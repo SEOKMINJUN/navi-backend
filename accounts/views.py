0625dc9bd3af74000f3d4a6e74be85bba2e1c4c3
SEOKMINJUN <smj00316@hanyang.ac.kr>
2025-05-30 22:18:00 +0900

    Add permission to reset password

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from .serializers import UserSignupSerializer
 from django.contrib.auth import get_user_model
 
@@ -19,14 +20,31 @@ class SignupView(APIView):
 # # 비밀번호 재설정 뷰
 
 class ResetPasswordView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def post(self, request):
-        username = request.data.get('username')
-        email = request.data.get('email')
-        new_password = request.data.get('new_password')
-        try:
-            user = User.objects.get(username=username, email=email)
-            user.set_password(new_password)
-            user.save()
+        # 관리자인 경우
+        if request.user.is_staff:
+            username = request.data.get('username')
+            new_password = request.data.get('new_password')
+            try:
+                user = User.objects.get(username=username)
+                user.set_password(new_password)
+                user.save()
+                return Response({'message': '비밀번호가 성공적으로 변경되었습니다.'}, status=status.HTTP_200_OK)
+            except User.DoesNotExist:
+                return Response({'message': '일치하는 사용자가 없습니다.'}, status=status.HTTP_404_NOT_FOUND)
+        
+        # 일반 사용자인 경우
+        else:
+            current_password = request.data.get('current_password')
+            new_password = request.data.get('new_password')
+            
+            # 현재 비밀번호 확인
+            if not request.user.check_password(current_password):
+                return Response({'message': '현재 비밀번호가 일치하지 않습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+            
+            # 새 비밀번호로 변경
+            request.user.set_password(new_password)
+            request.user.save()
             return Response({'message': '비밀번호가 성공적으로 변경되었습니다.'}, status=status.HTTP_200_OK)
-        except User.DoesNotExist:
-            return Response({'message': '일치하는 사용자가 없습니다.'}, status=status.HTTP_404_NOT_FOUND)
